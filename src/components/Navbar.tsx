b8bfcaf6f19bfa7011fee0997f3b6034990eb612
Si Thu Aung <sithu.a@siriustech.io>
2024-02-23 00:43:38 +0700

    Wrap Navbar items in Suspense component
    The 'Suspense' component from React has been implemented to wrap the Navbar items. This establishes a loading state while the components are being fetched, improving the overall application performance and user experience.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from "react";
+
 import NavbarItem from "@/components/NavbarItem";
 
 export default function Navbar() {
 	return (
 		<nav className="flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6">
-			<NavbarItem title="Trending" param="fetchTrending" />
-			<NavbarItem title="Top Rated" param="fetchTopRated" />
+			<Suspense>
+				<NavbarItem title="Trending" param="fetchTrending" />
+				<NavbarItem title="Top Rated" param="fetchTopRated" />
+			</Suspense>
 		</nav>
 	)
 }
